b8ff811977664227e1881bc52a1436ca0295a0f3
Javier Acosta <facosta.ie@gmail.com>
2013-08-30 10:12:48 -0700

    Nuevo archivo de prueba

---
+++ pruebaC++.cpp
@@ -0,0 +1,93 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+   
+    int n,m,res,sobra,tipo;
+     
+    
+    cout<<"Selecciona el tipo de operacion: \n";
+    cout<<"1.-Suma\n";
+    cout<<"2.-Resta\n";
+    cout<<"3.-Multiplicacion\n";
+    cout<<"4.-Division\n\n";
+    cin>>tipo;
+    
+    
+   
+           
+         if(tipo==1)
+            {
+               cout<<"Seleccionaste suma(+) \n";
+               cout<<"Introduce el primer numero: \n";
+               cin>>n;
+            
+              cout<<"Introduce el segundo numero: \n";
+               cin>>m;
+            
+               res<<n+m;
+              cout<<"el resultado es :  \n\n"<<res; 
+            
+            }
+            
+            
+          if(tipo==2)
+            {  
+               cout<<"Seleccionaste resta \n";
+               cout<<"Introduce el primer numero: \n";
+               cin>>n;
+        
+               cout<<"Introduce el segundo numero: \n";
+               cin>>m;
+        
+               res<<n-m;
+               cout<<"el resultado es :  \n\n"<<res;       	       
+            }
+           
+           
+           if(tipo==3)
+            {
+               cout<<"Seleccionaste multiplicacion \n";
+               cout<<"Introduce el primer numero: \n";
+               cin>>n;
+        
+               cout<<"Introduce el segundo numero: \n";
+               cin>>m;
+            
+               res<<n*m;
+               cout<<"el resultado es :  \n\n"<<res; 	             
+            }
+           
+           
+            if(tipo==4)
+            {
+              cout<<"Seleccionaste division \n";
+              cout<<"Introduce el primer numero: \n";
+              cin>>n;
+        
+              cout<<"Introduce el segundo numero: \n";
+              cin>>m;
+            
+              res<<n/m;
+              sobra<<n%m;
+                        if (res<=0)
+                         {
+                          cout<<"Error! division entre 0\n";          
+                           }
+              cout<<"El resultado es : \n\n"<<res; 	
+              cout<<"El residuo es: \n"<<sobra;             
+            }
+            
+            else
+            {
+            cout<<"ERROR No es una opcion valida \n";    
+            }
+           
+
+
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
